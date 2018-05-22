876d888e4e6136a65c0b8bcc49de98c107296f50
web155 <waki.alam@gmail.com>
2018-05-22 17:21:56 +0530

    waki_one

---
+++ customfont/src/main/java/com/wakim/customfont/CustomFontTextView.java
@@ -0,0 +1,48 @@
+package com.wakim.customfont;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class CustomFontTextView extends TextView {
+
+
+
+    public CustomFontTextView(Context context) {
+        this(context, null);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray array = context.obtainStyledAttributes(attrs,
+                R.styleable.CustomFontTextView, 0, 0);
+        String path_font = array.getString(R.styleable.CustomFontTextView_fontPath);
+        ColorStateList path_color = array.getColorStateList(R.styleable.CustomFontTextView_fontColor);
+
+        array.recycle();
+
+        setCustomTypeFace(context, path_font!=null ? path_font:"fonts/OpenSans-Regular.ttf");
+
+
+        setCustomTypeFaceColor(path_color!=null ? path_color:ColorStateList.valueOf(Color.BLACK));
+    }
+
+    private void setCustomTypeFace(Context context, String path_font) {
+        Typeface typeFaces = Typeface.createFromAsset(context.getAssets(), path_font);
+        this.setTypeface(typeFaces);
+    }
+
+
+    private void setCustomTypeFaceColor(ColorStateList path_color){
+        this.setTextColor(path_color);
+    }
+
+
+}
